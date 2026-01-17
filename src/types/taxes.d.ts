0c8907f019fcd2245a0ffdb897b1e9be59bb067a
Sands-45 <sandsqa@hotmail.com>
2026-01-17 15:18:56 +0200

    feat: Add WooTaxRateType interface

---
+++ src/types/taxes.d.ts
@@ -0,0 +1,27 @@
+export interface WooTaxRateType {
+  id: number;
+  country: string;
+  state: string;
+  postcode: string;
+  city: string;
+  rate: string;
+  name: string;
+  priority: number;
+  compound: boolean;
+  shipping: boolean;
+  order: number;
+  class: string;
+  postcodes: string[];
+  cities: string[];
+  _links: {
+    self: Array<{
+      href: string;
+      targetHints?: {
+        allow: string[];
+      };
+    }>;
+    collection: Array<{
+      href: string;
+    }>;
+  };
+}
